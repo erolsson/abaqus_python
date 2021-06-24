8657707a89730539ac5e1ff30b3c67e8eb5e614c
erolsson@kth.se <erolsson@kth.se>
2021-06-24 13:57:54 +0200

    "initial commit"

--- common.py
+++ common.py
@@ -3,22 +3,7 @@ import os
 import pathlib
 import shutil
 
-try:
-    import distro
-except ImportError:
-    import platform as distro
-
 package_path = os.path.dirname(__file__)
-
-if distro.linux_distribution()[0] == 'Ubuntu':
-    abq = 'singularity exec --nv ' + os.path.expanduser('~/imgs/sing/abaqus-2018-centos-7.img') + \
-          ' vglrun /opt/abaqus/2018/Commands/abq2018'
-    abq_viewer = 'singularity exec --nv ' + os.path.expanduser('~/imgs/sing/abaqus-2018-centos-7.img') + \
-                 ' vglrun -d :1 /opt/abaqus/2018/Commands/abq2018 viewer'
-else:
-    abq = '/scratch/users/erik/SIMULIA/CAE/2018/linux_a64/code/bin/ABQLauncher'
-    abq_viewer = '/scratch/users/erik/SIMULIA/CAE/2018/linux_a64/code/bin/ABQLauncher viewer'
-
 
 class TemporaryDirectory:
     def __init__(self, name):

b6a73e993ebe352100474309122a9f7cb1939a7b
erolsson@kth.se <erolsson@kth.se>
2021-07-09 17:43:03 +0200

    "Added path functions"

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='abaqus_python',
-    version='',
+    license='',
     install_requires=["numpy"],
     url='',
-    license='0.1',
+    version='0.1',
     author='erolsson',
-    packages=['abaqus_python'],
+    packages=['abaqus_interface'],
     author_email='erolsson@kth.se',
     description=''
 )
